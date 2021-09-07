3f3abfa7f42afc1681bd3bdb8aa99ce1e0cf7609
asallard <Aurore.Sallard@ivt.baug.ethz.ch>
2021-09-07 15:59:53 +0200

    Cleaning files

--- Data/impute_income.py
+++ Data/impute_income.py
@@ -6,13 +6,17 @@ def impute(df_mz):
     no_income_selector = df_mz["hhl_income10"] == -1
     df_mz["sex"] = 1
     df_mz.loc[df_mz["Sex"] == "Male", "sex"] = 2
-    df_mz["nb_cars"] = np.maximum(0, df_mz["nb_cars"])
 
     print(list(set(df_mz["nb_cars"].values.tolist())))
+    print(list(set(df_mz["nb_bikes"].values.tolist())))
+    print(list(set(df_mz["Age"].values.tolist())))
+    print(list(set(df_mz["sex"].values.tolist())))
+    print(list(set(df_mz["Marital_status"].values.tolist())))
+    print(list(set(df_mz["household_size"].values.tolist())))
 
     # TODO: We don't use any weights here. Shouldn't we?
     training_data = df_mz[~no_income_selector][[
-        "Age", "sex", "Marital_status", "household_size", "nb_cars", "nb_bikes"
+        "age_class9"#, "sex", "Marital_status", "household_size", "nb_cars"#, "nb_bikes"
     ]].values
 
     training_labels = df_mz[~no_income_selector]["hhl_income10"].values
@@ -25,7 +29,7 @@ def impute(df_mz):
 
     # Predict the incomes
     prediction_data = df_mz[no_income_selector][[
-        "Age", "sex", "Marital_status", "household_size", "nb_cars", "nb_bikes"
+        "age_class9"#, "sex", "Marital_status", "household_size", "nb_cars"#, "nb_bikes"
     ]].values
 
     df_mz.loc[no_income_selector, "hhl_income10"] = classifier.predict(prediction_data)
